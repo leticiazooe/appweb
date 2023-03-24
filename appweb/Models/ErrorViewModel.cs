cff1b6533973c0b27085feb078982ad3a0166c69
LETICIA SILVA <leticia.silva237@fatec.sp.gov.br>
2023-03-24 19:28:30 -0300

    Add project files.

---
+++ appweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
